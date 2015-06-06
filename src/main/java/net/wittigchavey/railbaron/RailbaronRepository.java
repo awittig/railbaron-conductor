228f90180ef476f3a67b3fda4cbac7734d79637e
awittig <awittig@gmail.com>
2015-06-05 21:49:50 -0500

    convert to gradle and spring boot

--- src/main/java/net/wittigchavey/railbaron/RailbaronRepository.java
+++ src/main/java/net/wittigchavey/railbaron/RailbaronRepository.java
@@ -1,48 +1,30 @@
 package net.wittigchavey.railbaron;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class RailbaronRepository {
 
 	private final List<Destination> destinations;
-	private NamedParameterJdbcOperations db;
-	
 
-	public RailbaronRepository() {
-		
-//		String dbName = System.getProperty("RDS_DB_NAME", "railbaron"); 
-//		String userName = System.getProperty("RDS_USERNAME", "user"); 
-//		String password = System.getProperty("RDS_PASSWORD", "password"); 
-//		String hostname = System.getProperty("RDS_HOSTNAME", "192.168.1.8");
-//		String port = System.getProperty("RDS_PORT", "3306");
-		
-		String dbName = System.getProperty("RDS_DB_NAME", "ebdb"); 
-		String userName = System.getProperty("RDS_USERNAME", "root"); 
-		String password = System.getProperty("RDS_PASSWORD", "password"); 
-		String hostname = System.getProperty("RDS_HOSTNAME", "aa18yb84gsgcv4n.czeagjx8cabn.us-west-2.rds.amazonaws.com");
-		String port = System.getProperty("RDS_PORT", "3306");
-		
-		DriverManagerDataSource dataSource = new DriverManagerDataSource () ;
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://"+hostname+":"+port+"/"+dbName);
-		dataSource.setUsername(userName);
-		dataSource.setPassword(password);
-		
-		db = new NamedParameterJdbcTemplate (dataSource);
-		
+	private final NamedParameterJdbcOperations db;
+
+	@Autowired
+	public RailbaronRepository(NamedParameterJdbcOperations db) {
+
+		this.db = db;
+
 		String sql = "select id, name from city";
 		RowMapper<Destination> rowMapper = BeanPropertyRowMapper.newInstance(Destination.class);
-		destinations = db.query(sql, rowMapper);
+		this.destinations = this.db.query(sql, rowMapper);
 	}
 	
 	public List<Destination> getAllDestinations() {
@@ -56,7 +38,7 @@ public class RailbaronRepository {
 			return 0;
 		}
 		
-		String sql = "select payoff from new_payoff"
+		String sql = "select payoff from payoff"
 				+ " where (location1 = :fromCityId or location2 = :fromCityId)"
 				+ " and (location1 = :toCityId or location2 = :toCityId)";
 		Map<String,Object> params = new HashMap<String,Object>();
